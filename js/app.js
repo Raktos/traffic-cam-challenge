7bb80088925f5f1aa3e32651031c6f4db552ec4b
Information School <ischoolit@uw.edu>
2014-11-19 13:06:18 -0800

    kinda sorta working almost not really

--- js/app.js
+++ js/app.js
@@ -11,3 +11,50 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+$(document).ready(function() {
+    var mapElem = document.getElementById('map');
+    var center = {lat: 47.6, lng: -122.3};
+    var markers = [];
+
+    var map = new google.maps.Map(mapElem, {
+        center: center,
+        zoom: 12
+    });
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+        .done(function(data) {
+            data.forEach(function(cam) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: Number(cam.location.latitude),
+                        lng: Number(cam.location.longitude)
+                    },
+                    map: map,
+                    cam: cam
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    var html = '<h2>' + cam.cameralabel + '</h2>';
+                    html += '<img src="' + cam.imageurl.url + '"/>';
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, this);
+                });
+
+                markers.push(marker);
+            });
+        })
+        .fail(function(err) {
+            console.log(err);
+        })
+        .always(function() {
+            $('#ajax-loader').fadeOut();
+        });
+
+    $('#search').bind('search keyup', function() {
+        $.each(markers, function() {
+            
+        });
+    });
+});
